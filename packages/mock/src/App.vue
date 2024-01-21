07db17c25f07b51e3290ad3e7b089dbfbdb0263d
QiYuOr2 <48339849+QiYuOr2@users.noreply.github.com>
2024-01-21 17:57:24 +0800

    entry

--- packages/mock/src/App.vue
+++ packages/mock/src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import Entry from './components/Entry.vue';
 
 </script>
 
 <template>
-
+  <Entry>
+    <div>1</div>
+  </Entry>
 </template>
 
 <style scoped>
-
+@import '@unocss/reset/tailwind.css';
 </style>
+
---
+++ packages/mock/src/components/Entry.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDraggable } from '@vueuse/core'
+import { ref } from 'vue';
+
+const entry = ref<HTMLElement | null>(null)
+const { style } = useDraggable(entry, {
+  initialValue: { x: 10, y: 200 },
+})
+</script>
+
+<template>
+  <div ref="entry" class="fixed text-base bg-base p2 rounded cursor-pointer z50 select-none shadow" :style="style">
+    <div v-if="true">入口</div>
+    <slot v-else></slot>
+  </div>
+</template>
