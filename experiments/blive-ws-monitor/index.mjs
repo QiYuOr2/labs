817c6638701a64be8ca63c3cbdcd78ea1982c271
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2026-05-10 18:32:10 +0800

    update

--- experiments/blive-ws-monitor/index.mjs
+++ experiments/blive-ws-monitor/index.mjs
@@ -1,48 +1,65 @@
 import { startListen } from "blive-message-listener";
+import dayjs from "dayjs";
 import { barkURL } from "./bark.mjs";
 import "dotenv/config";
 
+function timeMarker(timestamp) {
+  return dayjs(timestamp).format("YYYY/MM/DD HH:mm:ss");
+}
+
+function sendMessage(options) {
+  const time = timeMarker(options.timestamp);
+  console.log(`[${time}] ${options.title}`);
+  fetch(
+    barkURL({
+      title: `【blive-ws-monitor】${options.title}`,
+      content: options.content,
+    }),
+  );
+}
+
 function main() {
-  const instance = startListen(544786, {
-    onLiveStart: () => {
-      fetch(
-        barkURL({
-          title: "【blive-ws-monitor】直播开始提醒",
-          content: "12dora 直播开始了",
-        }),
-      );
+  // 544786 12dora
+  // 92613 api
+  const instance = startListen(92613, {
+    onLiveStart: (message) => {
+      if (message.raw['live_time']) {
+        sendMessage({
+          title: '直播开始提醒',
+          content: '12dora 来了',
+          timestamp: message.timestamp,
+        })
+      } else {
+        console.log(`[${timeMarker(message.timestamp)}] 推流LOG`)
+      }
     },
-    onLiveEnd: () => {
-      fetch(
-        barkURL({
-          title: "【blive-ws-monitor】直播结束提醒",
-          content: "12dora 直播结束了",
-        }),
-      );
+    onLiveEnd: (message) => {
+      sendMessage({
+        title: '直播结束提醒',
+        content: '12dora 溜了',
+        timestamp: message.timestamp,
+      });
     },
     onOpen: () => {
-      fetch(
-        barkURL({
-          title: "【blive-ws-monitor】监听开始",
-          content: "成功建立连接",
-        }),
-      );
+      sendMessage({
+        title: '监听开始',
+        content: '成功建立连接',
+        timestamp: Date.now(),
+      });
     },
     onClose: () => {
-      fetch(
-        barkURL({
-          title: "【blive-ws-monitor】监听结束",
-          content: "已断开直播间链接",
-        }),
-      );
+      sendMessage({
+        title: '监听结束',
+        content: '已断开直播间链接',
+        timestamp: Date.now(),
+      });
     },
     onError: (error) => {
-      fetch(
-        barkURL({
-          title: "【blive-ws-monitor】发生错误",
-          content: error.message,
-        }),
-      );
+      sendMessage({
+        title: "发生错误",
+        content: error.message,
+        timestamp: Date.now(),
+      });
     },
   });
 
